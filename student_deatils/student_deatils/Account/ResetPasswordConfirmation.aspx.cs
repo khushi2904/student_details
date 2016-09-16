9fdacd5edec7a692b08448e8fc99f19b93ed04ea
khushi2904 <khushdesai2904@gmail.com>
2016-09-16 11:36:31 +0530

    project template

---
+++ student_deatils/student_deatils/Account/ResetPasswordConfirmation.aspx.cs
@@ -0,0 +1,8 @@
+﻿using System.Web.UI;
+
+namespace student_deatils.Account
+{
+    public partial class ResetPasswordConfirmation : Page
+    {
+    }
+}
---
+++ student_deatils/student_deatils/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(student_deatils.Startup))]
+namespace student_deatils
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
