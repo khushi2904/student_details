29005299521099036b169714b3a961a8ddd4994b
khushi2904 <khushdesai2904@gmail.com>
2016-09-16 12:44:35 +0530

    update delete

--- student_deatils/student_deatils/Default.aspx
+++ student_deatils/student_deatils/Default.aspx
@@ -2,41 +2,22 @@
 
 <asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
 
-    <div class="jumbotron">
-        <h1>ASP.NET</h1>
-        <p class="lead">ASP.NET is a free web framework for building great Web sites and Web applications using HTML, CSS, and JavaScript.</p>
-        <p><a href="http://www.asp.net" class="btn btn-primary btn-lg">Learn more &raquo;</a></p>
-    </div>
+    <br /><br />
+    <asp:Button ID="btnupdate" runat="server" Text="Update" OnClick="btnupdate_Click" />
+    <asp:Button ID="btndelete" runat="server" Text="Delete" OnClick="btndelete_Click" />
+    <br /><br />
+    <asp:Label ID="lblname" runat="server" Text="Name : " Visible="false"></asp:Label>
+    <asp:TextBox ID="txtname" runat="server" Visible="false"></asp:TextBox>
+    
+    &nbsp;&nbsp;&nbsp;
+    
+    <asp:Label ID="lblmarks" runat="server" Text="Marks : " Visible="false"></asp:Label>
+    <asp:TextBox ID="txtmarks" runat="server" Visible="false"></asp:TextBox>
 
-    <div class="row">
-        <div class="col-md-4">
-            <h2>Getting started</h2>
-            <p>
-                ASP.NET Web Forms lets you build dynamic websites using a familiar drag-and-drop, event-driven model.
-            A design surface and hundreds of controls and components let you rapidly build sophisticated, powerful UI-driven sites with data access.
-            </p>
-            <p>
-                <a class="btn btn-default" href="http://go.microsoft.com/fwlink/?LinkId=301948">Learn more &raquo;</a>
-            </p>
-        </div>
-        <div class="col-md-4">
-            <h2>Get more libraries</h2>
-            <p>
-                NuGet is a free Visual Studio extension that makes it easy to add, remove, and update libraries and tools in Visual Studio projects.
-            </p>
-            <p>
-                <a class="btn btn-default" href="http://go.microsoft.com/fwlink/?LinkId=301949">Learn more &raquo;</a>
-            </p>
-        </div>
-        <div class="col-md-4">
-            <h2>Web Hosting</h2>
-            <p>
-                You can easily find a web hosting company that offers the right mix of features and price for your applications.
-            </p>
-            <p>
-                <a class="btn btn-default" href="http://go.microsoft.com/fwlink/?LinkId=301950">Learn more &raquo;</a>
-            </p>
-        </div>
-    </div>
+    <br /><br />
+    <asp:Label ID="lblerror" runat="server" Text="" Visible="false"></asp:Label>
+
+    <br /><br />
+    <asp:Button ID="btnsubmit" runat="server" Text="submit" Visible="false" OnClick="btnsubmit_Click" />
 
 </asp:Content>
--- student_deatils/student_deatils/Default.aspx.cs
+++ student_deatils/student_deatils/Default.aspx.cs
@@ -4,14 +4,96 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
 
 namespace student_deatils
 {
     public partial class _Default : Page
     {
+        SqlConnection conn;
+        int n = 0;
+
         protected void Page_Load(object sender, EventArgs e)
         {
+            string myconnectionstring = @"Data Source=(localdb)\Projects;Initial Catalog=master;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False";
+            conn = new SqlConnection(myconnectionstring);
 
+            if (!this.IsPostBack)
+            {
+                lblname.Visible = false;
+                lblmarks.Visible = false;
+                txtname.Visible = false;
+                txtmarks.Visible = false;
+                btnsubmit.Visible = false;
+                lblerror.Visible = false;
+                lblerror.Text = "";
+            }
+        }
+
+        protected void btnupdate_Click(object sender, EventArgs e)
+        {
+            lblerror.Text = "";
+            lblname.Visible = true;
+            lblmarks.Visible = true;
+            txtname.Visible = true;
+            txtmarks.Visible = true;
+            btnsubmit.Visible = true;
+            lblerror.Visible = true;
+            n = 1;
+           
+        }
+
+        protected void btnsubmit_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                lblerror.Text = "";
+                conn.Open();
+                string sqls;
+                SqlCommand cmd;
+                if (n == 1)
+                {
+                    sqls = "update student_results set marks=@marks where name='@name'";
+                    cmd = new SqlCommand(sqls, conn);
+                    cmd.Parameters.AddWithValue("@marks", txtmarks.Text);
+                    cmd.Parameters.AddWithValue("@name", txtname.Text);
+                }
+                else if (n == 2)
+                {
+                    sqls = "delete from student_results where name='@name'";
+                    cmd = new SqlCommand(sqls, conn);
+                    cmd.Parameters.AddWithValue("@name", txtname.Text);
+                }
+                else
+                {
+                    return;
+                }
+                cmd.ExecuteNonQuery();
+                lblerror.Text = "Update Successful.";
+
+            }
+            catch (Exception ex)
+            {
+                lblerror.Text = "Update Unsuccessful.";
+            }
+            finally
+            {
+                conn.Close();
+            }
+        }
+
+        protected void btndelete_Click(object sender, EventArgs e)
+        {
+            lblerror.Text = "";
+            lblmarks.Visible = false;
+            txtmarks.Visible = false;
+            lblname.Visible = true;
+            txtname.Visible = true;
+            btnsubmit.Visible = true;
+            lblerror.Visible = true;
+            n = 2;
         }
     }
 }
